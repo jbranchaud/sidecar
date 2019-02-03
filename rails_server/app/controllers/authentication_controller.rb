fde401f606400296847ebc449353a3358d623b9b
jbranchaud <jbranchaud@gmail.com>
2019-02-02 21:55:40 -0600

    Check user lookup for checking password
    Avoids nil method error exception

--- rails_server/app/controllers/authentication_controller.rb
+++ rails_server/app/controllers/authentication_controller.rb
@@ -2,9 +2,7 @@ class AuthenticationController < ApiController
   def check_password
     @user = User.find_by(email: params[:email])
 
-    puts "controller pass: #{params[:password]}"
-
-    if @user.check_password(params[:password])
+    if @user && @user.check_password(params[:password])
       render json: {email: params[:email], valid_pass: true}.to_json
     else
       render json: {email: params[:email], valid_pass: false}.to_json
