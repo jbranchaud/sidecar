6c0072d582ce4a6c9ff49fc72b70c7a7679ab4b4
jbranchaud <jbranchaud@gmail.com>
2019-02-04 14:37:15 -0600

    Extract SignIn module, display message on failure

--- cra_client/src/App.js
+++ cra_client/src/App.js
@@ -1,19 +1,10 @@
 import './css/base.css';
 
-import {
-  Pane,
-  TextInputField,
-  Button,
-  Alert,
-  Tab,
-  TabNavigation,
-  Heading,
-  Text,
-} from 'evergreen-ui';
-import { Router, Link, Location, navigate } from '@reach/router';
-import React, { Component } from 'react';
+import { Pane, Tab, TabNavigation, Heading, Text } from 'evergreen-ui';
+import { Router, Link, Location } from '@reach/router';
+import React from 'react';
 
-import SectionHeading from './components/SectionHeading';
+import SignIn from './SignIn';
 import SignUp from './SignUp';
 
 const ExactNavLink = props =>
@@ -107,108 +98,6 @@ class Home extends React.Component {
         );
       }
     }
-  }
-}
-
-class SignIn extends Component {
-  signIn = ({ email, password }) => {
-    fetch('/api/sign_in', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    })
-      .then(response => {
-        localStorage.setItem(
-          'auth_token',
-          response.headers.get('Authorization')
-        );
-        return response.json();
-      })
-      .then(json => {
-        this.setState({ passwordCheck: !!json['valid_pass'] });
-        navigate('/');
-      })
-      .catch(err => {
-        alert('Something went wrong!');
-        console.log(err);
-      });
-  };
-
-  state = {
-    email: '',
-    password: '',
-    passwordCheck: null,
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    this.signIn({
-      email: this.state.email,
-      password: this.state.password,
-    });
-  };
-
-  handleEmailChange = e => {
-    this.setState({ email: e.target.value });
-  };
-
-  handlePasswordChange = e => {
-    this.setState({ password: e.target.value });
-  };
-
-  render() {
-    const passwordCheckSucceeded =
-      this.state.passwordCheck !== null && this.state.passwordCheck;
-    const passwordCheckFailed =
-      this.state.passwordCheck !== null && !this.state.passwordCheck;
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <Pane display="flex" flexDirection="column" width="280px">
-          <SectionHeading>Sign In</SectionHeading>
-          {passwordCheckSucceeded &&
-            <Alert
-              appearance="card"
-              intent="success"
-              title="Your password is correct"
-              marginBottom={32}
-            />}
-          {passwordCheckFailed &&
-            <Alert
-              appearance="card"
-              intent="danger"
-              title="Your password is incorrect"
-              marginBottom={32}
-            />}
-          <TextInputField
-            label="Email"
-            type="text"
-            name="email"
-            onChange={this.handleEmailChange}
-            value={this.state.email}
-          />
-          <TextInputField
-            label="Password"
-            type="password"
-            name="password"
-            onChange={this.handlePasswordChange}
-            value={this.state.password}
-          />
-          <Button
-            intent="default"
-            type="submit"
-            onClick={this.handleSubmit}
-            justifyContent="center"
-          >
-            Sign In
-          </Button>
-        </Pane>
-      </form>
-    );
   }
 }
 
---
+++ cra_client/src/SignIn/index.js
@@ -0,0 +1,118 @@
+import { Alert, Button, Pane, TextInputField } from 'evergreen-ui';
+import { navigate } from '@reach/router';
+import React from 'react';
+
+import SectionHeading from '../components/SectionHeading';
+
+export const INITIAL_STATUS = 'initial_status';
+export const LOADING_STATUS = 'loading_status';
+export const FAILED_STATUS = 'failed_status';
+
+class SignIn extends React.Component {
+  state = {
+    status: INITIAL_STATUS,
+    message: null,
+    email: '',
+    password: '',
+  };
+
+  setLoadingStatus = () => {
+    this.setState({ status: LOADING_STATUS, message: null });
+  };
+
+  setFailedStatus = ({ message }) => {
+    this.setState({ status: FAILED_STATUS, message: message || 'Failure!' });
+  };
+
+  signIn = ({ email, password }) => {
+    this.setLoadingStatus();
+
+    fetch('/api/sign_in', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then(response => {
+        localStorage.setItem(
+          'auth_token',
+          response.headers.get('Authorization')
+        );
+        return response.json();
+      })
+      .then(json => {
+        if (json.error) {
+          this.setFailedStatus({ message: json.error });
+        } else {
+          navigate('/');
+        }
+      })
+      .catch(err => {
+        this.setFailedStatus({ message: 'Something went wrong!' });
+        console.log(err);
+      });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.signIn({
+      email: this.state.email,
+      password: this.state.password,
+    });
+  };
+
+  handleEmailChange = e => {
+    this.setState({ email: e.target.value });
+  };
+
+  handlePasswordChange = e => {
+    this.setState({ password: e.target.value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Pane display="flex" flexDirection="column" width="280px">
+          <SectionHeading>Sign In</SectionHeading>
+          {this.state.status === FAILED_STATUS &&
+            <Alert
+              appearance="card"
+              intent="danger"
+              title={this.state.message}
+              marginBottom={32}
+            />}
+          <TextInputField
+            label="Email"
+            type="text"
+            name="email"
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+            disabled={this.state.status === LOADING_STATUS}
+          />
+          <TextInputField
+            label="Password"
+            type="password"
+            name="password"
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+            disabled={this.state.status === LOADING_STATUS}
+          />
+          <Button
+            intent="default"
+            type="submit"
+            onClick={this.handleSubmit}
+            justifyContent="center"
+            disabled={this.state.status === LOADING_STATUS}
+          >
+            Sign In
+          </Button>
+        </Pane>
+      </form>
+    );
+  }
+}
+
+export default SignIn;
