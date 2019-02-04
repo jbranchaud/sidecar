3e024948211cdf6b9dbfdab31e73895afc9d172b
jbranchaud <jbranchaud@gmail.com>
2019-02-04 15:58:02 -0600

    Clean up footprint of sign_in method

--- rails_server/app/models/user_authentication.rb
+++ rails_server/app/models/user_authentication.rb
@@ -1,25 +1,18 @@
 class UserAuthentication
   attr_reader :token, :user, :error
 
-  def initialize(email, password)
-    @email = email
-    @password = password
-  end
-
   def self.sign_in(email, password)
-    user_auth = new(email, password)
-
-    user_auth.sign_in
-
-    user_auth
+    new.sign_in(email, password)
   end
 
-  def sign_in
-    @user = User.authenticate(@email, @password)
+  def sign_in(email, password)
+    @user = User.authenticate(email, password)
     if @user
-      @token = JsonWebToken.encode({ email: @email })
+      @token = JsonWebToken.encode({ email: email })
     else
       @error = "The email and password did not match"
     end
+
+    self
   end
 end
