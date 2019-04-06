d98c4dad90037900325127e0e437055f6ed025c5
jbranchaud <jbranchaud@gmail.com>
2019-04-06 17:58:27 -0500

    Add standardrb and run it against controller spec

--- rails_server/spec/controllers/authentication_controller_spec.rb
+++ rails_server/spec/controllers/authentication_controller_spec.rb
@@ -1,12 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe AuthenticationController do
-  describe 'POST #request_password_reset_link' do
-    context 'when user email is valid' do
-      it 'responds with a 200 and UUID token' do
+  describe "POST #request_password_reset_link" do
+    context "when user email is valid" do
+      it "responds with a 200 and UUID token" do
         user = User.create(email: "user1@example.com", password: "password")
 
-        post :request_password_reset_link, params: { email: user.email, format: :json}
+        post :request_password_reset_link,
+          params: {email: user.email, format: :json}
 
         expect(response.status).to eq(200)
 
@@ -15,29 +16,36 @@ describe AuthenticationController do
       end
     end
 
-    context 'when user email is not found' do
-      it 'responds with a 404' do
-        post :request_password_reset_link, params: { email: "nonexistant@example.com", format: :json}
+    context "when user email is not found" do
+      it "responds with a 404" do
+        post :request_password_reset_link,
+          params: {email: "nonexistant@example.com", format: :json}
 
         expect(response.status).to eq(404)
       end
     end
   end
 
-  describe 'PUT #reset_password' do
+  describe "PUT #reset_password" do
     let(:user) {
       User.create(email: "user1@example.com", password: "password")
     }
     let(:reset_token) {
-        reset_token = SecureRandom.uuid
-        PasswordResetToken.create(user_id: user.id, reset_token: reset_token)
-        reset_token
+      reset_token = SecureRandom.uuid
+      PasswordResetToken.create(user_id: user.id, reset_token: reset_token)
+      reset_token
     }
     let(:new_password) { "new_password" }
 
-    context 'when reset token is valid' do
+    context "when reset token is valid" do
       it "responds with a 200 and updates the user's password" do
-        put :reset_password, params: { reset_token: reset_token, password: new_password, password_confirmation: new_password, format: :json}
+        put :reset_password,
+          params: {
+            reset_token: reset_token,
+            password: new_password,
+            password_confirmation: new_password,
+            format: :json,
+          }
 
         expect(response.status).to eq(200)
 
@@ -45,21 +53,32 @@ describe AuthenticationController do
       end
     end
 
-    context 'when reset token is valid and passwords do not match' do
+    context "when reset token is valid and passwords do not match" do
       it "responds with a 400" do
         mismatched_password = "mismatched_password"
 
-        put :reset_password, params: { reset_token: reset_token, password: new_password, password_confirmation: mismatched_password, format: :json}
+        put :reset_password,
+          params: {
+            reset_token: reset_token,
+            password: new_password,
+            password_confirmation: mismatched_password,
+            format: :json,
+          }
 
         expect(response.status).to eq(400)
       end
     end
 
-    context 'when reset token is invalid' do
+    context "when reset token is invalid" do
       it "responds with a 404" do
         some_token = SecureRandom.uuid
 
-        put :reset_password, params: { reset_token: some_token, password: new_password, password_confirmation: new_password, format: :json}
+        put :reset_password, params: {
+          reset_token: some_token,
+          password: new_password,
+          password_confirmation: new_password,
+          format: :json,
+        }
 
         expect(response.status).to eq(404)
       end
