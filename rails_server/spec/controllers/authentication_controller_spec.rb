4d0f643932902ad18357091cec5fa6edbacd81e2
jbranchaud <jbranchaud@gmail.com>
2019-04-12 21:38:07 -0500

    Move send_email matcher into support file

--- rails_server/spec/controllers/authentication_controller_spec.rb
+++ rails_server/spec/controllers/authentication_controller_spec.rb
@@ -1,16 +1,6 @@
 require "rails_helper"
 include ActionMailer::TestHelper
 
-require 'rspec/expectations'
-
-RSpec::Matchers.define :send_email do |mailer_action|
-  match do |mailer_class|
-    message_delivery = instance_double(ActionMailer::MessageDelivery)
-    expect(mailer_class).to receive(mailer_action).and_return(message_delivery)
-    allow(message_delivery).to receive(:deliver_later)
-  end
-end
-
 describe AuthenticationController do
   describe "POST #request_password_reset_link" do
     before(:each) do
