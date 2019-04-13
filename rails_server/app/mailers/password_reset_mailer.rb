33af91c95c7d15acc6b7ff2f18674da97af64967
jbranchaud <jbranchaud@gmail.com>
2019-04-13 10:17:43 -0500

    Link up the password reset link in HTML email

--- rails_server/app/mailers/password_reset_mailer.rb
+++ rails_server/app/mailers/password_reset_mailer.rb
@@ -1,7 +1,7 @@
 class PasswordResetMailer < ApplicationMailer
   def default_email(user, reset_record)
     @user = user
-    @reset_record = reset_record
+    @password_reset_url = "http://localhost:3000/password-reset/#{reset_record.reset_token}"
 
     mail(to: @user.email, subject: 'Password Reset Link')
   end
